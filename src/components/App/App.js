dbac54a021ca069537d0f6f1e8fa38141f0c8af7
Daniel <Daniel.clement37@gmail.com>
2021-06-21 22:59:07 -0500

    refactored board html and added pepeScrap image

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,20 @@
 
 import './App.css';
 import '../SudokuBoard/SudokuBoard.css';
-import '../SudokuBoard/SudokuTile/SudokuTile.css';
 import '../NumPad/NumPad.css';
+import React from 'react';
+import Tile from '../SudokuBoard/SudokuTile/SudokuTile';
+import PepeScrap from '../../images/pepeScrap.png'
 
-function App() {
+const App = ()=> {
+  const tiles = () =>{
+    const tilesState = [];
+    for (let i = 0; i <  81; i++) {
+      tilesState.push(<Tile/>)  
+    }
+    return tilesState;
+  }
+
   return (
     <div className="flex content-center justify-center App bg-primary ">
       <div className="flex-auto w-2/3 justify-self-center max-w-2/3 App-container bg-secondary">
@@ -13,90 +23,13 @@ function App() {
         </header>
         <div className="Game-container">
           <div className="Board-container">
-            <div className="tile"><h2>1</h2></div>
-            <div className="tile"><h2>1</h2></div>
-            <div className="tile"><h2>1</h2></div>
-            <div className="tile"><h2>1</h2></div>
-            <div className="tile"><h2>1</h2></div>
-            <div className="tile"><h2>1</h2></div>
-            <div className="tile"><h2>1</h2></div>
-            <div className="tile"><h2>1</h2></div>
-            <div className="tile"><h2>1</h2></div>
-            <div className="tile"><h2>1</h2></div>
-            <div className="tile"><h2>1</h2></div>
-            <div className="tile"><h2>1</h2></div>
-            <div className="tile"><h2>1</h2></div>
-            <div className="tile"><h2>1</h2></div>
-            <div className="tile"><h2>1</h2></div>
-            <div className="tile"><h2>1</h2></div>
-            <div className="tile"><h2>1</h2></div>
-            <div className="tile"><h2>1</h2></div>
-            <div className="tile"><h2>1</h2></div>
-            <div className="tile"><h2>1</h2></div>
-            <div className="tile"><h2>1</h2></div>
-            <div className="tile"><h2>1</h2></div>
-            <div className="tile"><h2>1</h2></div>
-            <div className="tile"><h2>1</h2></div>
-            <div className="tile"><h2>1</h2></div>
-            <div className="tile"><h2>1</h2></div>
-            <div className="tile"><h2>1</h2></div>
-            <div className="tile"><h2>1</h2></div>
-            <div className="tile"><h2>1</h2></div>
-            <div className="tile"><h2>1</h2></div>
-            <div className="tile"><h2>1</h2></div>
-            <div className="tile"><h2>1</h2></div>
-            <div className="tile"><h2>1</h2></div>
-            <div className="tile"><h2>1</h2></div>
-            <div className="tile"><h2>1</h2></div>
-            <div className="tile"><h2>1</h2></div>
-            <div className="tile"><h2>1</h2></div>
-            <div className="tile"><h2>1</h2></div>
-            <div className="tile"><h2>1</h2></div>
-            <div className="tile"><h2>1</h2></div>
-            <div className="tile"><h2>1</h2></div>
-            <div className="tile"><h2>1</h2></div>
-            <div className="tile"><h2>1</h2></div>
-            <div className="tile"><h2>1</h2></div>
-            <div className="tile"><h2>1</h2></div>
-            <div className="tile"><h2>1</h2></div>
-            <div className="tile"><h2>1</h2></div>
-            <div className="tile"><h2>1</h2></div>
-            <div className="tile"><h2>1</h2></div>
-            <div className="tile"><h2>1</h2></div>
-            <div className="tile"><h2>1</h2></div>
-            <div className="tile"><h2>1</h2></div>
-            <div className="tile"><h2>1</h2></div>
-            <div className="tile"><h2>1</h2></div>
-            <div className="tile"><h2>1</h2></div>
-            <div className="tile"><h2>1</h2></div>
-            <div className="tile"><h2>1</h2></div>
-            <div className="tile"><h2>1</h2></div>
-            <div className="tile"><h2>1</h2></div>
-            <div className="tile"><h2>1</h2></div>
-            <div className="tile"><h2>1</h2></div>
-            <div className="tile"><h2>1</h2></div>
-            <div className="tile"><h2>1</h2></div>
-            <div className="tile"><h2>1</h2></div>
-            <div className="tile"><h2>1</h2></div>
-            <div className="tile"><h2>1</h2></div>
-            <div className="tile"><h2>1</h2></div>
-            <div className="tile"><h2>1</h2></div>
-            <div className="tile"><h2>1</h2></div>
-            <div className="tile"><h2>1</h2></div>
-            <div className="tile"><h2>1</h2></div>
-            <div className="tile"><h2>1</h2></div>
-            <div className="tile"><h2>1</h2></div>
-            <div className="tile"><h2>1</h2></div>
-            <div className="tile"><h2>1</h2></div>
-            <div className="tile"><h2>1</h2></div>
-            <div className="tile"><h2>1</h2></div>
-            <div className="tile"><h2>1</h2></div>
-            <div className="tile"><h2>1</h2></div>
-            <div className="tile"><h2>1</h2></div>
-            <div className="tile"><h2>1</h2></div>
+            {tiles()}
           </div>
           <div className="Side-controls-container"></div>
           <div className="Number-Btns"></div>
+          <div className="image-container">
+            <img src={PepeScrap} alt="pepe scrappy" className="pepe-scrap"/>
+          </div>
         </div>
       </div>
     </div>
