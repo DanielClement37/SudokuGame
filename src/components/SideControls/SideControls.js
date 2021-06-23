a14cf83e924eac34be5c6aa4ae52b38b0e019689
Daniel <Daniel.clement37@gmail.com>
2021-06-23 00:43:12 -0500

    started work on the side controls and timer along with a few css details on the board

--- src/components/SideControls/SideControls.js
+++ src/components/SideControls/SideControls.js
@@ -4,7 +4,22 @@ import './SideControls.css'
 export default function SideControls() {
     return (
         <div className="side-controls">
-            
+            <div className="timer-container">
+                <div className="timer-item">
+                    <h3>EASY</h3>
+                </div>
+                <div className="timer-item">
+                    <h1>03:13</h1>
+                </div>
+            </div>
+            <div className="controls-container">
+                <div className="undo-btn"></div>
+                <div className="hint-btn"></div>
+                <div className="notes-btn"></div>
+                <div className="eraser-btn"></div>
+                <div className="new-game-btn"></div>
+                <div className="settings-btn"></div>
+            </div>
         </div>
     )
 }
