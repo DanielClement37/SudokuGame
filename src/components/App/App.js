e7d36c8f67482b0321910fcb34f1f8744ef44eb8
Daniel <Daniel.clement37@gmail.com>
2021-07-05 12:39:10 -0500

    working on board initialization

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,32 +1,33 @@
+import "./App.css";
+import React from "react";
+import SudokuBoard from "../SudokuBoard/SudokuBoard";
+import NumPad from "../NumPad/NumPad";
+import { GameContextProvider } from "../../contexts/GameContext";
+import SideControls from "../SideControls/SideControls";
+import PepeScrap from "../../images/pepeScrap.png";
 
-import './App.css';
-import React from 'react';
-import SudokuBoard from '../SudokuBoard/SudokuBoard';
-import NumPad from '../NumPad/NumPad';
-import SideControls from '../SideControls/SideControls';
-import PepeScrap from '../../images/pepeScrap.png';
-//import BoardGenerator from '../../utils/BoardGenerator';
-
-const App = ()=> {
-  
-
+const App = () => {
   return (
     <div className="flex content-center justify-center App bg-primary ">
-      <div className="flex-auto w-2/3 justify-self-center max-w-2/3 App-container bg-secondary">
-        <header className="App-header">
-          <div className="text-8xl tracking-header header-title text-bold text-primary">SUDOKU</div>
-        </header>
-        <div className="Game-container">
-          <SudokuBoard />
-          <NumPad />
-          <SideControls />
-          <div className="image-container">
-            <img src={PepeScrap} alt="pepe scrappy" className="pepe-scrap"/>
+      <GameContextProvider>
+        <div className="flex-auto w-2/3 justify-self-center max-w-2/3 App-container bg-secondary">
+          <header className="App-header">
+            <div className="text-8xl tracking-header header-title text-bold text-primary">
+              SUDOKU
+            </div>
+          </header>
+          <div className="Game-container">
+            <SudokuBoard />
+            <NumPad />
+            <SideControls />
+            <div className="image-container">
+              <img src={PepeScrap} alt="pepe scrappy" className="pepe-scrap" />
+            </div>
           </div>
         </div>
-      </div>
+      </GameContextProvider>
     </div>
   );
-}
+};
 
 export default App;
