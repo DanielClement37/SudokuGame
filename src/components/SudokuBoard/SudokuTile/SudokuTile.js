c5bfa77f782468de9c2ee9f7aac3d36999596e33
Daniel <Daniel.clement37@gmail.com>
2021-07-11 23:31:20 -0500

    when value is 0 display does not bug out anymore

--- src/components/SudokuBoard/SudokuTile/SudokuTile.js
+++ src/components/SudokuBoard/SudokuTile/SudokuTile.js
@@ -2,7 +2,6 @@ import "./SudokuTile.css";
 import React from "react";
 import { useStore } from "../../../store/Store";
 import { classNames } from "../../../utils/classNames.ts";
-import { getColWord } from "../../../utils/Conveter";
 
 const Tile = (props) => {
   const [state] = useStore();
@@ -11,33 +10,51 @@ const Tile = (props) => {
   let tileValue = "";
   if (props.value === 0) {
     tileValue = " ";
+    return (
+      <div
+        className="tile"
+        id={props.tileId}
+        onClick={(e) => {
+          props.selectTileHandler(props);
+        }}
+      >
+        <div
+          className={classNames(
+            "value-container",
+            props.row === selectedTile.row && "selected-row-class",
+            props.col === selectedTile.col && "selected-col-class"
+          )}
+        >
+          {tileValue}
+        </div>
+      </div>
+    );
   } else {
     tileValue = props.value;
-  }
-
-  return (
-    <div
-      className="tile"
-      id={props.tileId}
-      onClick={(e) => {
-        props.selectTileHandler(props);
-      }}
-    >
+    return (
       <div
-        className={
-          classNames("value-container",
-          props.row === selectedTile.row &&
-            props.col === selectedTile.col &&
-            "selected-tile-class",
-          props.row === selectedTile.row && "selected-row-class",
-          props.col === selectedTile.col && "selected-col-class",
-          props.value === selectedTile.value && "selected-value-class")
-        }
+        className="tile"
+        id={props.tileId}
+        onClick={(e) => {
+          props.selectTileHandler(props);
+        }}
       >
-        {tileValue}
+        <div
+          className={classNames(
+            "value-container",
+            props.row === selectedTile.row &&
+              props.col === selectedTile.col &&
+              "selected-tile-class",
+            props.row === selectedTile.row && "selected-row-class",
+            props.col === selectedTile.col && "selected-col-class",
+            props.value === selectedTile.value && "selected-value-class"
+          )}
+        >
+          {tileValue}
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 };
 
 export default Tile;
